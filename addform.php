b74fb2cd4b66d8414f2c7e2288b488f957324b41
imat19 <vergara.reymart82@gmail.com>
2023-03-31 20:37:17 +0800

    add form

--- addform.php
+++ addform.php
@@ -22,7 +22,6 @@
             <button type="submit" class="btn btn-primary mt-2">Submit</button>
           </div>
         </form>
-  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
   </body>
 </html>
